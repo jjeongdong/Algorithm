c3fff2b3ef401c791ffc3db25f63c93c1a46954c
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2023-05-06 23:32:59 +0900

    [Bronze V] Title: A-B, Time: 124 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,8 @@
+class Main{
+	public static void main(String[] args) throws java.io.IOException{
+		int a = System.in.read() - '0';
+		System.in.read();
+		int b = System.in.read() - '0';
+		System.out.println(a-b);
+	}
+}
