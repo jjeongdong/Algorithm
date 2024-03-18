93ff3eb49e79499ce5b529d4aa73ad50fb5c599f
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2024-03-19 01:06:38 +0900

    [Silver II] Title: 쇠막대기, Time: 176 ms, Memory: 16388 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        Stack<Character> stack = new Stack<>();
+        int answer = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') {
+                stack.push('(');
+            } else {
+                if (str.charAt(i - 1) == '(') {
+                    stack.pop();
+                    answer += stack.size();
+                } else {
+                    stack.pop();
+                    answer++;
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
