7f356c832bc2950ad4c75e1ab871cacaa9a22e34
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2024-03-27 01:46:45 +0900

    [Gold IV] Title: 오큰수, Time: 980 ms, Memory: 144444 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            while (!stack.empty() && arr[stack.peek()] < arr[i]) {
+                arr[stack.pop()] = arr[i];
+            }
+
+            stack.push(i);
+        }
+
+        while(!stack.isEmpty()) {
+            arr[stack.pop()] = -1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N; i++) {
+            sb.append(arr[i]).append(' ');
+        }
+
+        System.out.println(sb);
+    }
+}
