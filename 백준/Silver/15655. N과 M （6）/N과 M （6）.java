2cef7188575a675b71c05c6c0c5eb804cb180abc
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2024-05-13 11:33:13 +0900

    [Silver III] Title: N과 M (6), Time: 128 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    static int N, M;
+    static int[] arr;
+    static int[] numArr;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st1.nextToken());
+        M = Integer.parseInt(st1.nextToken());
+
+        numArr = new int[N];
+        arr = new int[M];
+
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            numArr[i] = Integer.parseInt(st2.nextToken());
+        }
+
+        Arrays.sort(numArr);
+
+        T.DFS(0, 0);
+        System.out.print(sb);
+    }
+
+    void DFS(int L, int S) {
+        if (L == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+        } else {
+            for (int i = S; i < N; i++) {
+                arr[L] = numArr[i];
+                DFS(L + 1, i + 1);
+            }
+        }
+    }
+}
+
