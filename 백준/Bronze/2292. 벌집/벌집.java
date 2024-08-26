bf548341223f289683bc21a8b0a8e7fcb1189303
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2024-08-26 21:32:15 +0900

    [Bronze II] Title: 벌집, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -1,25 +1,26 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.io.IOException;
+ 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
+ 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		
-		System.out.print(solution(n));
-	}
-	
-	private static int solution(int n) {
-		if(n == 1) return 1;
-		int i=2;
-		int k=1;
-		
-		while(i<=n) {
-			i += 6*k++;
+ 
+		int N = Integer.parseInt(br.readLine());
+		int count = 1; // 겹 수(최소 루트)
+		int range = 2;	// 범위 (최솟값 기준) 
+ 
+		if (N == 1) {
+			System.out.print(1);
 		}
-		
-		return k;
+ 
+		else {
+			while (range <= N) {	// 범위가 N보다 커지기 직전까지 반복 
+				range = range + (6 * count);
+				count++;
+			}
+			System.out.print(count);
+		}
 	}
 }
