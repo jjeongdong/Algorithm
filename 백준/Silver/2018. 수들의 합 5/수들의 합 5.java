5885163a6d8cbb4dbe857ea60b2593a6515206a2
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2024-11-11 22:29:45 +0900

    [Silver V] Title: 수들의 합 5, Time: 148 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -7,23 +7,22 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        int count = 0; // 경우의 수를 세기 위한 변수
-        int sum = 0; // 연속된 자연수의 합
+        int count = 1; // 경우의 수를 세기 위한 변수
+        int sum = 1; // 연속된 자연수의 합
         int start = 1; // 시작 포인터
+        int end = 1;
 
-        // start부터 N까지 연속된 합을 구하면서 경우의 수를 세기
-        for (int end = 1; end <= N; end++) {
-            sum += end;
-
-            // 합이 N보다 크면, start를 증가시켜서 합을 줄임
-            while (sum > N) {
-                sum -= start;
-                start++;
-            }
-
-            // 합이 N과 같으면 경우의 수 증가
+        while (end != N) {
             if (sum == N) {
                 count++;
+                end++;
+                sum = sum + end;
+            } else if (sum > N) {
+                sum = sum - start;
+                start++;
+            } else {
+                end++;
+                sum = sum + end;
             }
         }
 
