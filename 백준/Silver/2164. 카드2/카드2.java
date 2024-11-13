e7c07bd6a34f4deb0fd1f1a091369014c082533c
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2024-11-13 22:17:38 +0900

    [Silver IV] Title: 카드2, Time: 168 ms, Memory: 45456 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,28 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-import java.util.*;
+/*
+9 8
+CCTGGATTG
+2 0 1 1
+ */
 
 public class Main {
-    public static void main(String[] args) {
+    static int[] myArr = new int[4];
+    static int[] checkArr = new int[4];
+    static int answer = 0;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         Queue<Integer> queue = new LinkedList<>();
 
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= N ; i++) {
             queue.add(i);
         }
 
         while (true) {
-
             if (queue.size() == 1) {
                 System.out.println(queue.poll());
                 break;
             }
-            
+
             queue.poll();
             queue.add(queue.poll());
         }
-
     }
 }
