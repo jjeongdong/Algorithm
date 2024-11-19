7cb379dc5b10acb10326c2b9dbdea1828d106d28
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2024-11-19 11:35:13 +0900

    [Gold V] Title: 두 용액, Time: 316 ms, Memory: 28100 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -1,47 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-// 두 용액
-// 이진탐색 비슷한 문제
+
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(arr);
-		
-		int i = 0;
-		int j = arr.length - 1;
-		
-		int gap = Integer.MAX_VALUE;
-		int ans1 = 0;
-		int ans2 = 0;
-		
-		int temp;
-		int sum;
-		while (i < j) {
-			sum = arr[i] + arr[j];
-			temp = Math.abs(sum);
-			if (temp < gap) {
-				gap = temp;
-				ans1 = arr[i];
-				ans2 = arr[j];
-			}
-			if (sum > 0)
-				j--;
-			else
-				i++;
-		}
-		
-		System.out.println(ans1 + " " + ans2);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int gap = Integer.MAX_VALUE;
+        int MAX = 0;
+        int MIN = 0;
+        int lt = 0;
+        int rt = arr.length - 1;
+
+        Arrays.sort(arr);
+
+        while (lt < rt) {
+            int sum = arr[lt] + arr[rt];
+            int tmp = Math.abs(sum);
+
+            if (tmp < gap) {
+                gap = tmp;
+                MAX = arr[rt];
+                MIN = arr[lt];
+            }
+
+
+            if (sum < 0) {
+                lt++;
+            } else {
+                rt--;
+            }
+        }
+
+        sb.append(MIN).append(" ").append(MAX);
+        System.out.println(sb);
+    }
 }
