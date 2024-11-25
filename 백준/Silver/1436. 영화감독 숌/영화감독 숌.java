187ad9c6a248fd70439615e7763563a3cc8688e6
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2024-11-25 14:19:27 +0900

    [Silver V] Title: 영화감독 숌, Time: 260 ms, Memory: 86028 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,22 +1,30 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
- 
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
- 
-		int num = 666;
-		int count = 1;
-        
-		while(count != N) {
-			num++;
-			if(String.valueOf(num).contains("666")) {
-				count++;
-			}
-		}
-		System.out.println(num);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int tmp = 666;
+        int index = 1;
+
+        while (true) {
+            if (String.valueOf(tmp).contains("666")) {
+                if (index == N) break;
+                else index++;
+            }
+            tmp++;
+        }
+
+        System.out.println(tmp);
+    }
 }
