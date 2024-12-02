a87a817d4901f03e4f681176bb0f02b39b6b3c7c
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2024-12-02 20:44:31 +0900

    [Silver III] Title: 피보나치 함수, Time: 100 ms, Memory: 14096 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        int[][] arr = new int[41][2];
+
+        arr[0][0] = 1;
+        arr[0][1] = 0;
+        arr[1][0] = 0;
+        arr[1][1] = 1;
+
+        for (int i = 2; i <= 40; i++) {
+            arr[i][0] = arr[i - 1][0] + arr[i - 2][0];
+            arr[i][1] = arr[i - 1][1] + arr[i - 2][1];
+        }
+
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            sb.append(arr[N][0]).append(" ").append(arr[N][1]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
