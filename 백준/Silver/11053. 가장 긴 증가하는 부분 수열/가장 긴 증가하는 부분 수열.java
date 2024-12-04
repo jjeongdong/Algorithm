1b2bf895e65e7729c1622853e91580131377ec72
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2024-12-04 19:22:28 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 124 ms, Memory: 14512 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,37 +1,34 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n;
-    static int[] arr;
-    static int[] dp;
-    static int max = Integer.MIN_VALUE;
-    static int answer = 0;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n];
-        dp = new int[n];
-
-        dp[0] = 1;
-
+        int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
+        int[] arr = new int[N + 1];
+        int[] dp = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 1; i < n; i++) {
-            int max = 0;
-            for (int j = 0; j < i; j++) {
-                if (arr[i] > arr[j]) {
-                    max = Math.max(max, dp[j]);
+        dp[1] = 1;
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j < i; j++) {
+                if (arr[i] > arr[j] && dp[i] < dp[j] + 1) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
-            dp[i] = max + 1;
+
+            if (dp[i] == 0) dp[i] = 1;
         }
 
+        int answer = Integer.MIN_VALUE;
         for (int tmp : dp) {
             answer = Math.max(answer, tmp);
         }
