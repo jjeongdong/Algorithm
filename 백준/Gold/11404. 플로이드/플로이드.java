9e244f45b8c67a38cb7a55ea0d4ac7fe7536821d
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2024-12-20 11:44:00 +0900

    [Gold IV] Title: 플로이드, Time: 512 ms, Memory: 45188 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());    // 도시의 개수
+        int M = Integer.parseInt(br.readLine());    // 버스의 개수
+        long[][] distance = new long[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i == j) distance[i][j] = 0;
+                else distance[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            distance[start][end] = Math.min(distance[start][end], weight);
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    distance[i][j] = Math.min(distance[i][j], distance[i][k] + distance[k][j]);
+                }
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (distance[i][j] == Integer.MAX_VALUE) {
+                    System.out.print("0 ");
+                } else {
+                    System.out.print(distance[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
