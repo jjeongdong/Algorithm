609624da2ab2916500376b62ffc4d1592506faf2
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2025-04-05 23:56:34 +0900

    [Bronze V] Title: A/B, Time: 184 ms, Memory: 17980 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
-
+ 
 public class Main {
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        double a = sc.nextDouble();
-        double b = sc.nextDouble();
-
-        sc.close();
-        System.out.print(a/b);
-
-    }
+ 
+	public static void main(String[] args) {
+ 
+		Scanner in=new Scanner(System.in);
+		
+		double a=in.nextDouble();
+		double b=in.nextDouble();
+		
+		in.close();
+		System.out.print(a/b);
+ 
+	}
 }
