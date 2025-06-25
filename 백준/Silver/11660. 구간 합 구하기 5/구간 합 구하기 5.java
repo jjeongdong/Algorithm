9d40569837e04deff4ed72fa7c994553c45364b2
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2025-06-25 20:20:17 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 788 ms, Memory: 123436 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,9 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/*
+4 4
+1 2 3 4
+2 3 4 5
+3 4 5 6
+4 5 6 7
+2 2 3 4
+3 4 3 4
+1 1 4 4
+1 3 4 4
+ */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,7 +24,7 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
 
         int[][] arr = new int[N + 1][N + 1];
-        int[][] sum = new int[N + 1][N + 1];
+        int[][] S = new int[N + 1][N + 1];
 
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -23,10 +33,9 @@ public class Main {
             }
         }
 
-        // 합배열 저장
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + arr[i][j];
+                S[i][j] = S[i - 1][j] + S[i][j - 1] - S[i - 1][j - 1] + arr[i][j];
             }
         }
 
@@ -37,11 +46,13 @@ public class Main {
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
 
-            if (x1 == x2 && y1 == y2) {
+            if (x1 == 0 && y1 == 0) {
+                sb.append(S[x2][y2]).append("\n");
+            } else if (x1 == x2 && y1 == y2) {
                 sb.append(arr[x1][y1]).append("\n");
             } else {
-                sb.append(sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1])
-                        .append("\n");
+                int value = S[x2][y2] - S[x1 - 1][y2] - S[x2][y1 - 1] + S[x1 - 1][y1 - 1];
+                sb.append(value).append("\n");
             }
         }
 
