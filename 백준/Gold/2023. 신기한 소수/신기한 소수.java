904ca2ae69147e179891970d104e4eef28e86447
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2025-06-28 16:50:37 +0900

    [Gold V] Title: 신기한 소수, Time: 696 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        DFS(2, 1);
+        DFS(3, 1);
+        DFS(5, 1);
+        DFS(7, 1);
+
+        System.out.println(sb);
+    }
+
+    private static void DFS(int number, int jarisu) {
+        if (jarisu == N) {
+            if (isPrime(number)) {
+                sb.append(number).append("\n");
+            }
+        }
+
+        for (int i = 1; i < 10; i += 2) {
+            if (isPrime(number * 10 + i)) {
+                DFS(number * 10 + i, jarisu + 1);
+            }
+        }
+    }
+
+    private static boolean isPrime(int number) {
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
