2b3e0e725792928c9d021ab36af3c4b0f7e81f4c
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2025-06-30 15:23:19 +0900

    [Silver I] Title: 단지번호붙이기, Time: 112 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -2,68 +2,82 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-
     static int N;
-    static int size;
     static int[][] board;
-    static boolean[][] visit;
-    static int[] dx = new int[]{-1, 1, 0, 0};
-    static int[] dy = new int[]{0, 0, -1, 1};
+    static boolean[][] visited;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static ArrayList<Integer> A;
 
     public static void main(String[] args) throws IOException {
-        Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        List<Integer> list = new ArrayList<>();
-        int answer = 0;
-
         N = Integer.parseInt(br.readLine());
-
         board = new int[N][N];
-        visit = new boolean[N][N];
+        visited = new boolean[N][N];
+        A = new ArrayList<>();
+        int count = 0;
 
         for (int i = 0; i < N; i++) {
             String str = br.readLine();
             for (int j = 0; j < N; j++) {
-                board[i][j] = Integer.parseInt(String.valueOf(str.charAt(j)));
+                board[i][j] = str.charAt(j) - '0';
             }
         }
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (board[i][j] == 1 && !visit[i][j]) {
-                    size = 1;
-                    T.DFS(i, j);
-                    list.add(size);
+                if (board[i][j] == 1 && !visited[i][j]) {
+                    visited[i][j] = true;
+                    bfs(i, j);
+                    count++;
                 }
             }
         }
 
-        System.out.println(list.size());
-        Collections.sort(list);
-
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
+        System.out.println(count);
+        Collections.sort(A);
+        for (int i : A) {
+            System.out.println(i);
         }
     }
 
-    public void DFS(int x, int y) {
-        visit[x][y] = true;
+    private static void bfs(int x, int y) {
+        int area = 1;
+        Queue<Point> queue = new LinkedList<>();
+        queue.offer(new Point(x, y));
 
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+        while (!queue.isEmpty()) {
+            Point now = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
 
-            if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
-                if (board[nx][ny] == 1 && !visit[nx][ny]) {
-                    size++;
-                    DFS(nx, ny);
+                if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
+                    if (!visited[nx][ny] && board[nx][ny] == 1) {
+                        area++;
+                        visited[nx][ny] = true;
+                        queue.offer(new Point(nx, ny));
+                    }
                 }
             }
+        }
+
+        A.add(area);
+    }
+
+    static class Point {
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
 }
