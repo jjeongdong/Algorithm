988a0f3cc435867ab20daf67de9e3d7533aa319c
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2025-06-30 15:40:22 +0900

    [Silver II] Title: 촌수계산, Time: 96 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -1,66 +1,73 @@
+
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    static int N;
+    static int startNum;
+    static int targetNum;
+    static int M;
+    static ArrayList<Integer>[] A;
+    static int[] dist;
+    static boolean[] visited;
+    static boolean flag = false;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
+        N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int x = Integer.parseInt(st.nextToken());
-        int y = Integer.parseInt(st.nextToken());
 
-        int M = Integer.parseInt(br.readLine());
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        startNum = Integer.parseInt(st.nextToken());
+        targetNum = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(br.readLine());
+        A = new ArrayList[N + 1];
+        dist = new int[N + 1];
+        visited = new boolean[N + 1];
 
-        for (int i = 0; i <= N; i++) {
-            graph.add(new ArrayList<>()); // 각 정점을 위한 리스트 초기화
+        for (int i = 1; i <= N; i++) {
+            A[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int node1 = Integer.parseInt(st.nextToken());
-            int node2 = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
 
-            graph.get(node1).add(node2);
-            graph.get(node2).add(node1);
+            A[s].add(e);
+            A[e].add(s);
         }
 
-        int L = 0;
-        boolean flag = false;
-        boolean[] visited = new boolean[N + 1];
-        Queue<Integer> queue = new LinkedList<>();
+        bfs();
+        if (flag) System.out.println(dist[targetNum]);
+        else System.out.println(-1);
+    }
 
-        visited[x] = true;
-        queue.offer(x);
+    private static void bfs() {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(startNum);
+        dist[startNum] = 0;
+        visited[startNum] = true;
 
         while (!queue.isEmpty()) {
-            int len = queue.size();
-            for (int i = 0; i < len; i++) {
-                int now = queue.poll();
-
-                int size = graph.get(now).size();
-                for (int j = 0; j < size; j++) {
-                    int tmp = graph.get(now).get(j);
-
-                    if (tmp == y) {
-                        flag = true;
-                        break;
-                    }
-
-                    if (!visited[tmp]) {
-                        visited[tmp] = true;
-                        queue.offer(tmp);
-                    }
-                }
+            int now = queue.poll();
+            if (now == targetNum) {
+                flag = true;
+                break;
             }
 
-            if (flag) break;
-            L++;
+            for (int i : A[now]) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    queue.offer(i);
+                    dist[i] = dist[now] + 1;
+                }
+            }
         }
-
-        if (!flag) System.out.println(-1);
-        else System.out.println(L + 1);
     }
 }
