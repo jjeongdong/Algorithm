4c3bc0e2987974c8783f6e7ee208d3e0a49f89fe
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2025-07-01 11:30:22 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 116 ms, Memory: 14836 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -1,18 +1,27 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static ArrayList<Integer>[] A;
     static int N;
     static int M;
-
+    static int[] distance;
+    static boolean[] visited;
+    static ArrayList<Integer>[] A;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int min = Integer.MAX_VALUE;
+        int answer = 0;
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
         A = new ArrayList[N + 1];
+
         for (int i = 1; i <= N; i++) {
             A[i] = new ArrayList<>();
         }
@@ -26,45 +35,40 @@ public class Main {
             A[e].add(s);
         }
 
-        int answer = 0;
-        int min = Integer.MAX_VALUE;
-
         for (int i = 1; i <= N; i++) {
             int sum = bfs(i);
-            if (sum < min) {
+            if (min > sum) {
                 min = sum;
                 answer = i;
             }
         }
 
         System.out.println(answer);
+
     }
 
-    private static int bfs(int start) {
+    private static int bfs(int number) {
         Queue<Integer> queue = new LinkedList<>();
-        boolean[] visited = new boolean[N + 1];
-        int[] dist = new int[N + 1];
+        int sum = 0;
+        distance = new int[N + 1];
+        visited = new boolean[N + 1];
+        visited[number] = true;
 
-        queue.offer(start);
-        visited[start] = true;
-
+        queue.offer(number);
         while (!queue.isEmpty()) {
-            int now = queue.poll();
-
-            for (int next : A[now]) {
-                if (!visited[next]) {
-                    visited[next] = true;
-                    dist[next] = dist[now] + 1;
-                    queue.offer(next);
+            Integer now = queue.poll();
+            for (int i : A[now]) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    distance[i] = distance[now] + 1;
+                    queue.offer(i);
                 }
             }
         }
 
-        int sum = 0;
         for (int i = 1; i <= N; i++) {
-            sum += dist[i];
+            sum += distance[i];
         }
-
         return sum;
     }
 }
