561d5125ae794ae921d60c27da8aa4b8154af311
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2025-07-03 18:52:46 +0900

    [Silver I] Title: 경로 찾기, Time: 156 ms, Memory: 16716 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,48 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        long[][] graph = new long[N + 1][N + 1];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // i에서 j까지 갈 수 있는가?
+        // i에서 k로 가고, k에서 j로 갈 수 있는가?
+        // 위에 2개의 질문은 동일함.
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    // 단순히 갈 수 있는 경로가 있는지만 체크함.
+                    if (graph[i][k] == 1 && graph[k][j] == 1) {
+                        graph[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                sb.append(graph[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+
+        System.out.println(sb);
+    }
+}
