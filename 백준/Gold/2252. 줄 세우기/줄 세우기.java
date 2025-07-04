5463033bb05f31bb658608d98f04ff40cb75962b
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2025-07-04 11:50:47 +0900

    [Gold III] Title: 줄 세우기, Time: 436 ms, Memory: 48416 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -3,40 +3,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 
 public class Main {
-    static int[][] arr;
-    static boolean[] visit;
-    static int N, M, V;
-    static StringBuilder sb = new StringBuilder();
-
     public static void main(String[] args) throws IOException {
-        Main T = new Main();
-        StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        ArrayList<ArrayList<Integer>> A = new ArrayList<>();
-        for (int i = 0; i <= N; i++) {
-            A.add(new ArrayList<>());
+
+        ArrayList<Integer>[] A = new ArrayList[N + 1];
+        int[] indegree = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            A[i] = new ArrayList<>();
         }
 
-        int[] indegree = new int[N + 1];
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int S = Integer.parseInt(st.nextToken());
-            int E = Integer.parseInt(st.nextToken());
-            A.get(S).add(E);
-            indegree[E]++;  // 진입 차수 배열 데이터 저장 부분
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            A[start].add(end);
+            indegree[end]++;
         }
 
-        // 위상 정렬 실행
         Queue<Integer> queue = new LinkedList<>();
         for (int i = 1; i <= N; i++) {
             if (indegree[i] == 0) {
@@ -45,12 +41,12 @@ public class Main {
         }
 
         while (!queue.isEmpty()) {
-            int now = queue.poll();
+            Integer now = queue.poll();
             sb.append(now).append(" ");
-            for (int next : A.get(now)) {
+            for (int next : A[now]) {
                 indegree[next]--;
                 if (indegree[next] == 0) {
-                    queue.offer(next);
+                    queue.add(next);
                 }
             }
         }
