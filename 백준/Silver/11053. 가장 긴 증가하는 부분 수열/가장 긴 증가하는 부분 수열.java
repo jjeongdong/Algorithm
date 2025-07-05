bd63227758f7aea12a752db54536cec44c643ff9
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2025-07-05 15:29:20 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 120 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,36 +1,34 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] arr = new int[N + 1];
-        int[] dp = new int[N + 1];
+        int[] dp = new int[N];
+        int[] arr = new int[N];
 
-        for (int i = 1; i <= N; i++) {
+        Arrays.fill(dp, 1);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        dp[1] = 1;
-
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j < i; j++) {
-                if (arr[i] > arr[j] && dp[i] < dp[j] + 1) {
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
-
-            if (dp[i] == 0) dp[i] = 1;
         }
 
-        int answer = Integer.MIN_VALUE;
-        for (int tmp : dp) {
-            answer = Math.max(answer, tmp);
+
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            answer = Math.max(answer, dp[i]);
         }
 
         System.out.println(answer);
