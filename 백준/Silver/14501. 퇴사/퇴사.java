c1c5058fc38795a180c0484ef492b33939a427cd
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2025-07-06 14:43:08 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,45 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.PriorityQueue;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-
 public class Main {
-
-    static int answer = Integer.MIN_VALUE;
-    static int N;
-    static int[][] arr;
-
     public static void main(String[] args) throws IOException {
-        Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N][2];
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[] T = new int[N];
+        int[] P = new int[N];
+        int[] dp = new int[N + 1];
 
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
+
+            T[i] = t;
+            P[i] = p;
         }
 
-        T.DFS(0, 0);
-        System.out.println(answer);
-    }
-
-    private void DFS(int L, int S) {
-        if (L >= N) {
-            answer = Math.max(S, answer);
-            return;
+        for (int i = 0; i < N; i++) {
+            if (i + T[i] <= N && dp[i] + P[i] > dp[i + T[i]]) {
+                dp[i + T[i]] = dp[i] + P[i];
+            }
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
         }
 
-        if (L + arr[L][0] <= N) {
-            DFS(L + arr[L][0], S + arr[L][1]);
-        } else {
-            DFS(L + arr[L][0], S);
-        }
-
-        DFS(L + 1, S);
+        System.out.println(dp[N]);
     }
 }
