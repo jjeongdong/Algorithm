eac7164b09ac9a35fb2b148c48ab2a55881f0016
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2025-07-14 16:30:18 +0900

    [Silver III] Title: N과 M (2), Time: 100 ms, Memory: 14416 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,46 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-
 public class Main {
-
-    static int N, M;
+    static int N;
+    static int M;
     static int[] arr;
-    static int[] ch;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
-        Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st1.nextToken());
-        M = Integer.parseInt(st1.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
         arr = new int[M];
-        ch = new int[30];
+        visited = new boolean[N + 1];
+        DFS(1, 0);
 
-        T.DFS(0, 1);
+        System.out.println(sb);
     }
 
-    void DFS(int L, int S) {
+    public static void DFS(int num, int L) {
         if (L == M) {
-            for (int tmp : arr) {
-                System.out.print(tmp + " ");
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
             }
-            System.out.println();
+            sb.append("\n");
         } else {
-            for (int i = S; i <= N; i++) {
-                if (ch[i] == 0) {
+            for (int i = num; i <= N; i++) {
+                if (!visited[i]) {
                     arr[L] = i;
-                    ch[i] = 1;
-                    DFS(L + 1, i + 1);
-                    ch[i] = 0;
+                    visited[i] = true;
+                    DFS(i + 1, L + 1);
+                    visited[i] = false;
                 }
             }
-
         }
     }
 }
