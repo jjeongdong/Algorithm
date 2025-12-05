842dc2b66433916c8d6a4fa91e42980a809d3eab
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2025-12-05 19:04:25 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 18268 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,51 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int x = Integer.parseInt(br.readLine());
-        int L = 0;
-        Queue<Integer> Q = new LinkedList<>();
-        int[] ch = new int[x + 1];
-        ch[x] = 1;
-        Q.offer(x);
 
-        while (!Q.isEmpty()) {
-            int len = Q.size();
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
 
-            for (int i = 0; i < len; i++) {
-                int v = Q.poll();
+        dp[1] = 0;
 
-                if (v == 1) {
-                    System.out.println(L);
-                    return;
-                }
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + 1;
 
-                if (v % 3 == 0 && ch[v / 3] == 0) {
-                    Q.offer(v / 3);
-                    ch[v / 3] = 1;
-                }
-
-                if (v % 2 == 0 && ch[v / 2] == 0) {
-                    Q.offer(v / 2);
-                    ch[v / 2] = 1;
-                }
-
-                if (ch[v - 1] == 0) {
-                    Q.offer(v - 1);
-                    ch[v - 1] = 1;
-                }
-
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
             }
-
-            L++;
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
         }
 
-
+        System.out.println(dp[N]);
     }
 }
